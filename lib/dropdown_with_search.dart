c7bf3be263bf3389dd73ae6b2e01efd839d9ecc8
Amarchand K <unofficial00345@gmail.com>
2023-05-22 22:22:39 +0530

    60

--- lib/dropdown_with_search.dart
+++ lib/dropdown_with_search.dart
@@ -65,7 +65,7 @@ class DropdownWithSearch<T> extends StatelessWidget {
           });
         },
         child: Container(
-          height: 70,
+          height: 60,                                                  
           padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
           decoration: !disabled
               ? decoration != null
